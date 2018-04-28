8a2406781a2a979c0d480754b1b062f9c3b6b205
Martín Montes Pérez <martin.montes@nomasystems.com>
2018-04-28 17:41:49 +0200

    Updated version: Added node resolver

--- index.js
+++ index.js
@@ -5,12 +5,30 @@ module.exports = {
     node: true
   },
   extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["import", "prettier"],
   rules: {
+    "import/extensions": [
+      "error",
+      "always",
+      {
+        js: "never",
+        node: "never"
+      }
+    ],
     "import/no-dynamic-require": 0,
     "import/prefer-default-export": 0,
     "no-underscore-dangle": 0,
     "no-console": 1,
     "prettier/prettier": "error",
   },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [
+          ".js",
+          ".node"
+        ]
+      }
+    }
+  }
 };
